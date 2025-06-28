a5a951c826554eccfe24601b634c9ed5ddd0b896
shahar-evergreen <shahar.duany@evergreenfinance.co.uk>
2025-06-28 12:27:53 +0300

    removed tests for low input

--- test/util.test.js
+++ test/util.test.js
@@ -18,19 +18,5 @@ describe("dummyFunction(input, logger)", () => {
         expect(result).toBe("high");
       });
     });
-
-    describe("is less or equal 5", () => {
-      it('should return "low" for input less than 5', () => {
-        const input = Math.floor(Math.random() * 5); // generates 0, 1, 2, 3, or 4
-        const result = dummyFunction(input);
-        expect(result).toBe("low");
-      });
-
-      it('should return "low" for input equals 5', () => {
-        const input = 5;
-        const result = dummyFunction(input);
-        expect(result).toBe("low");
-      });
-    });
   });
 });
