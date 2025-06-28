3059f41ee09b7b064b1ebd524bf89490a2edc87c
shahar-evergreen <shahar.duany@evergreenfinance.co.uk>
2025-06-28 12:34:05 +0300

    removed all tests at main

--- test/util.test.js
+++ test/util.test.js
@@ -2,13 +2,5 @@ import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
 import { dummyFunction } from "../src/util.js";
 
 describe("dummyFunction(input, logger)", () => {
-  describe("when the input", () => {
-    describe("is not a number", () => {
-      it("should return null", () => {
-        const input = {};
-        const result = dummyFunction(input);
-        expect(result).toBe(null);
-      });
-    });
-  });
+  it("TBD");
 });
